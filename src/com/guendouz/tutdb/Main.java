76439971eee47e48a0749dc7af173001db3a36cf
Fakri <tfakri@gmail.com>
2015-02-14 19:39:08 +0100

    Suppression

---
+++ src/com/guendouz/tutdb/Main.java
@@ -0,0 +1,20 @@
+package com.guendouz.tutdb;
+
+import java.sql.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Connexion connexion = new Connexion("Database.db");
+		connexion.connect();
+		
+		Book book = new Book("9aa883e6-dfc0-4149-902a-4dbdfa22a408",
+				"Système D'Information", "", 450, Date.valueOf("2012-11-01"),
+				"");
+		System.out.println(book.getPublished());
+		connexion.addBook(book);
+
+		connexion.close();
+	}
+
+}
